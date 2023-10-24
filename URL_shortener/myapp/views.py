a48c71ed4b7248a8b58b72233c0f4d4107beeb31
Aviansh <130178693+GitAviansh003@users.noreply.github.com>
2023-10-24 08:55:09 +0530

    deleting files

--- URL_shortener/myapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render #page load
-from django.http import HttpResponse  #for string load
-
-    
-# Create your views here.
-def home_page(request):
-   return render( request,"index.html") 
--- project/project/app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
-def hello_world(request):
-    return render(request,'task.html')
-
-# Create your views here.
