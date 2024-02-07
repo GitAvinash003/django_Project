0fbbbb13d789bf4af7179789a4c98a572023742f
Aviansh <130178693+GitAviansh003@users.noreply.github.com>
2024-02-07 20:21:57 +0530

    changes

--- Django Crud Project/Crud/Crud/urls.py
+++ Django Crud Project/Crud/Crud/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from mysite import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name="index"),
+    path('index/',views.index,name="index"),
     path('edit/<int:id>',views.edit,name="edit"),
     path('update/<int:id>',views.update,name='update'),
     path('delete/<int:id>', views.delete, name='delete'),
